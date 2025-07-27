80c6810ec433977d60b9afff96a109a72f45c819
aka-mikejs01 <michaelgadissa@gmail.com>
2025-07-27 21:22:50 +0300

    Project setup

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import logger from "./middleware/logger.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  morgan("combined", { stream: { write: (msg) => logger.http(msg.trim()) } })
+);
+
+const PORT = process.env.PORT || 8000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () =>
+      logger.info(`server running on port: http://localhost:${PORT}`)
+    );
+  } catch (err) {
+    logger.error(err.message);
+  }
+};
+
+startServer();
